64ba662c289035aab182538612c07c4e5fee78b0
dmalembe <malembedidier24@gmail.com>
2021-05-14 23:10:51 +0200

    Backend project initialisation

---
+++ backendtest/src/main/java/com/sender/backendtest/BackendtestApplication.java
@@ -0,0 +1,13 @@
+package com.sender.backendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendtestApplication.class, args);
+	}
+
+}
---
+++ backendtest/src/test/java/com/sender/backendtest/BackendtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sender.backendtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
